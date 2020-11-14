708061bdfb765699ec715f3b9bbaaf9ece2ab52f
konaonline <blackeu4@naver.com>
2020-11-15 01:14:43 +0900

    초기 커밋.

---
+++ TestEngine/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestEngine.Startup))]
+namespace TestEngine
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
